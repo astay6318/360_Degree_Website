8a813ab6b55ea8873cb97dffb1272062033b84d3
akshat-22 <aanshujaiswal135@gmail.com>
2023-10-29 14:42:38 +0530

    Integration of file_upload

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,10 @@ from .serializers import ImgaeStoreSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
-
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import SessionAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 @csrf_exempt
 @api_view(['POST'])
@@ -23,7 +26,7 @@ def register(request):
             # User.role = request.POST.get('role')
             user.save()
             login(request, user)
-            return JsonResponse({'message': 'Registration successful'})
+            return JsonResponse({'message': 'Registration successful'},status=200)
         # username=request.data('username')
         # obj=User.obhjects.create(username=,email=,password=)
         # return JsonResponse()
@@ -33,31 +36,67 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-@csrf_exempt
-def user_login(request):
-    if request.method == 'POST':
-        studentform = StudentLoginForm(data=request.POST)
-        teacherform = TeacherLoginForm(data=request.POST)
-        if studentform.is_valid():
-            user = studentform.get_user()
-            login(request,user)
-            if user.role == 'student':
-                return redirect('student_dashboard')
-            else:
-                return redirect('teacher_dashboard')
-        if teacherform.is_valid():
-            user = teacherform.get_user()
-            login(request, user)
-            if user.role == 'student':
-                return redirect('student_dashboard')
-            elif user.role == 'teacher':
-                return redirect('teacher_dashboard')
-    else:
-        studentform = StudentLoginForm()
-        teacherform = TeacherLoginForm()
-    return render(request, 'registration/login.html', {'student_form': studentform, 'teacher_form': teacherform,})
+# @csrf_exempt
+# def user_login(request):
+#     if request.method == 'POST':
+#         studentform = StudentLoginForm(data=request.POST)
+#         teacherform = TeacherLoginForm(data=request.POST)
+#         if studentform.is_valid():
+#             user = studentform.get_user()
+#             login(request,user)
+#             token, created = Token.objects.get_or_create(user=user)
+#             if user.role == 'student':
+#                 return redirect('student_dashboard',status=200,token=token.key)
+#             else:
+#                 return redirect('teacher_dashboard',status=200,token=token.key)
+#         if teacherform.is_valid():
+#             user = teacherform.get_user()
+#             login(request, user)
+#             token, created = Token.objects.get_or_create(user=user)
+#             if user.role == 'student':
+#                 return redirect('student_dashboard',status=200,token=token.key)
+#             elif user.role == 'teacher':
+#                 return redirect('student_dashboard',status=200,token=token.key)
+#     else:
+#         studentform = StudentLoginForm()
+#         teacherform = TeacherLoginForm()
+#     return render(request, 'registration/login.html', {'student_form': studentform, 'teacher_form': teacherform,})
+
+
+# @csrf_exempt
+# def custom_login(request):
+#     if request.method == 'POST':
+#         studentform = StudentLoginForm(data=request.POST)
+#         teacherform = TeacherLoginForm(data=request.POST)
+#         if studentform.is_valid():
+#             user = studentform.get_user()
+#             login(request, user)
+#             refresh = RefreshToken.for_user(user)
+#             token = str(refresh.access_token)
+#             return JsonResponse({'access_token': token, 'refresh_token': str(refresh)})
+#             # if user.role == 'student':
+#             #     return redirect('student_dashboard', token=token)
+#             # else:
+#             #     return redirect('teacher_dashboard', token=token)
+#         if teacherform.is_valid():
+#             user = teacherform.get_user()
+#             login(request, user)
+#             refresh = RefreshToken.for_user(user)
+#             token = str(refresh.access_token)
+#             return JsonResponse({'access_token': token, 'refresh_token': str(refresh)})
+#             # if user.role == 'student':
+#             #     return redirect('student_dashboard', token=token)
+#             # elif user.role == 'teacher':
+#             #     return redirect('teacher_dashboard', token=token)
+#     else:
+#         studentform = StudentLoginForm()
+#         teacherform = TeacherLoginForm()
+#         return JsonResponse({'error': 'Invalid credentials'}, status=400)
+#     # return render(request, 'registration/login.html', {'student_form': studentform, 'teacher_form': teacherform})
+
 
 class RandomViewSet(viewsets.ModelViewSet):
     queryset = ImageStore.objects.all()
     serializer_class = ImgaeStoreSerializer
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = (SessionAuthentication,)
+    # permission_classes = [IsAuthenticated]
