d8ec5f2e768e4dc7af5f3a25b63796e2878b70f7
akshat-22 <aanshujaiswal135@gmail.com>
2023-11-30 07:27:54 +0530

    Created backend for teacher dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -16,3 +16,29 @@ class ImageStore(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+class Teacher(models.Model):
+    # id = models.AutoField(primary_key=True)
+    name= models.CharField(max_length=500)
+    designation = models.CharField(max_length=500)
+    subject = models.CharField(max_length=500)
+    photo = models.FileField(null=True,blank=True,upload_to="teachers")
+    
+    def __str__(self):
+        return self.name
+    
+class Lesson(models.Model):
+    title = models.CharField(max_length=500)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='lessons')
+
+    def __str__(self):
+        return self.title
+
+class SubChapter(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.FileField(upload_to="subchapters",blank=True,null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE,default=0)
+    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE,related_name="subchapters")
+
+    def __str__(self):
+        return f"{self.lesson.title}-{self.title}"
