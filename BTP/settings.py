1be6a2f1dfa08da73e37c323ec82fcd118356ca1
astay6318 <aanshujaiswal135@gmail.com>
2024-03-16 15:57:36 +0530

    Vercel

--- BTP/settings.py
+++ BTP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(1))kiuoxtt@=6m^z+#-v693r7drxefa)1($_z*v8sqc+89)va
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
